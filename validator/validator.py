98e8dc62d91666628fa0e3a6a40d7e81017ee538
Sirajus Salekin <ssalek14@earlham.edu>
2023-07-07 23:13:41 -0400

    WIP: implementing a validator script with Python and json_schema

---
+++ validator/validator.py
@@ -0,0 +1,18 @@
+import json
+from jsonschema import validate, ValidationError
+
+def validate_resume(resume):
+    with open("resume_schema.json", "r") as schema_file:
+        schema = json.load(schema_file)
+        
+    try:
+        validate(resume, schema)
+        print("Resume is valid.")
+    except ValidationError as e:
+        print("Validation error:", e)
+
+with open("example.json", "r") as f:
+	resume_data = json.load(f)
+
+validate_resume(resume_data)
+
